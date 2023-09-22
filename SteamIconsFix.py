4cd2527cce11388506a7da7d54c561d845433037
Raj <havokentity@gmail.com>
2023-09-22 20:13:00 +0530

    Email update

--- SteamIconsFix.py
+++ SteamIconsFix.py
@@ -2,7 +2,7 @@
 --------------------------------------------------------------------------------
 Script Name: Steam Icons Fix
 Author: Rajesh Peter Douglas D'Monte
-Email: havokentity@gmail.com
+Email: dudenstein.games@gmail.com, rajeshdmonte@gmail.com
 
 This Python script downloads game icons for installed or specific Steam games.
 
